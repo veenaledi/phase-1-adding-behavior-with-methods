54fb74a39c7e5d7f6c6cddd387ee9cf5751176c9
neema <naledineema@gmail.com>
2024-07-22 15:30:03 +0300

    passed!

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+class Cat {
+    constructor(Sasha) {
+        this.name = Sasha;
+    }
+
+    speak() {
+        return `${this.name} says meow!`;
+    }
+}
+  
+  class Dog {
+    constructor(Rex) {
+        this.name = Rex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird {
+    constructor(Birdy, Pedro) {
+        this.name = Birdy;
+        this.isMale = Pedro;
+    }
+    speak() {
+            if (this.isMale) {
+                return `It's me! ${this.name}, the parrot!`;
+            } else {
+                return `${this.isMale} says squawk!`;
+            }
+        }
+          /*  constructor(Parrot, Birdy) {
+                this.isMale = Parrot;
+                this.name = Birdy;
+        
+            }
+            speak() {
+                return ` It's me! ${this.isMale}, the parrot!`
+            }
+            speak() {
+                return `${this.name} says squawk!`
+            }*/
+    }
